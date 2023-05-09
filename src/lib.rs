68643fb1512d71fe0ef8428881287e17603a2ddb
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2023-05-08 23:00:55 -0400

    Added documentation, less  blocks

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,22 @@
+#![crate_name="testlib_rust"]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#[doc(inline)]
 
 use std::{mem, ffi::CStr};
 
-include!("./bindings.rs");
+#[path = "bindings.rs"] mod CTestLib;
 
 #[derive(Debug)]
 pub enum TestLibPattern {
-    pattern_ab = _TESTLIB_PATTERN_PATTERN_AB as isize,
-    pattern_xy = _TESTLIB_PATTERN_PATTERN_XY as isize,
-    pattern_tp = _TESTLIB_PATTERN_PATTERN_TP as isize,
+    pattern_ab = CTestLib::_TESTLIB_PATTERN_PATTERN_AB as isize,
+    pattern_xy = CTestLib::_TESTLIB_PATTERN_PATTERN_XY as isize,
+    pattern_tp = CTestLib::_TESTLIB_PATTERN_PATTERN_TP as isize,
 }
 
 #[derive(Debug)]
+/// TestLibInfo struct
 pub struct TestLibInfo {
     pub name: String,
     pub cameraId: i32,
@@ -25,9 +28,17 @@ pub struct TestLibInfo {
 }
 
 impl TestLibInfo {
+    /// Returns info from TestLib
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// use testlib_rust::TestLibInfo;
+    /// let info = TestLibInfo::getInfo();
+    /// ```
     pub fn getInfo() -> Option<TestLibInfo> {
-        let mut info_prv: _TESTLIB_INFO = unsafe {mem::zeroed()};
-        let result = unsafe {testlib_getinfo(&mut info_prv)};
+        let mut info_prv: CTestLib::_TESTLIB_INFO = unsafe {mem::zeroed()};
+        let result = unsafe {CTestLib::testlib_getinfo(&mut info_prv)};
         match result {
             0 => {
                 let nstr = unsafe {CStr::from_ptr(info_prv.Name.as_ptr()).to_str().unwrap_or("")};
@@ -40,10 +51,10 @@ impl TestLibInfo {
                     maxWidth: info_prv.MaxWidth as i32,
                     isColorCam: info_prv.IsColorCam != 0,
                     bayerPattern: match info_prv.BayerPattern {
-                        _TESTLIB_PATTERN_PATTERN_AB => Some(TestLibPattern::pattern_ab),
-                        _TESTLIB_PATTERN_PATTERN_XY => Some(TestLibPattern::pattern_xy),
-                        _TESTLIB_PATTERN_PATTERN_TP => Some(TestLibPattern::pattern_tp),
-                        _TESTLIB_PATTERN_PATTERN_NONE => None,
+                        CTestLib::_TESTLIB_PATTERN_PATTERN_AB => Some(TestLibPattern::pattern_ab),
+                        CTestLib::_TESTLIB_PATTERN_PATTERN_XY => Some(TestLibPattern::pattern_xy),
+                        CTestLib::_TESTLIB_PATTERN_PATTERN_TP => Some(TestLibPattern::pattern_tp),
+                        CTestLib::_TESTLIB_PATTERN_PATTERN_NONE => None,
                         _ => None
                     }
                 };
@@ -65,8 +76,8 @@ mod tests {
         println!("{:?}", var);
         assert!(match var {
             None => false,
-            Some(x) => {
-                println!("{:?}", x.name.as_bytes());
+            Some(_x) => {
+                // println!("{:?}", x.name.as_bytes());
                 true
             }
         });
